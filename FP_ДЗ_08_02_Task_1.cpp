9d33c9ab62f1789f4f9becc62d51ad4d5a386599
Vladimir <79203161+Primus-max@user.noreply.github.com>
2024-02-11 07:17:10 +0300

    Добавьте файлы проекта.

---
+++ FP_ДЗ_08_02_Task_1.cpp
@@ -0,0 +1,39 @@
+﻿// В одномерном массиве, заполненном случайными числами, определить минимальный и максимальный элементы
+
+#include <iostream>
+#include <windows.h>
+#include <random>
+using namespace std;
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    const int size = 10;
+    int arr[size];
+
+    random_device random;
+    cout << "В массиве :" << endl ;
+
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = random() % 50;
+        cout << arr[i] << " " << endl;
+    }
+
+    int min = arr[0], max = arr[0];
+
+    for (int i = 0; i < size; i++)
+    {      
+       if (arr[i] < min)
+           min = arr[i];
+       else if (arr[i] > max)
+           max = arr[i];       
+    }
+
+
+    cout << "Минимальное : " << min << endl;
+    cout << "Максимальное : " << max << endl;
+}
+
